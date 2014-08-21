ac9d6b9366ed82a63f407d57517af71783473675
Christoph Burschka <christoph@burschka.de>
2014-08-21 15:59:28 +0200

    comments in Spring class

--- src/main/java/physics/Spring.java
+++ src/main/java/physics/Spring.java
@@ -8,13 +8,23 @@ public class Spring {
     private final double length;
     private final double strength;
     
+    /**
+     * Create a new spring.
+     * @param length The equilibrium length.
+     * @param strength The stiffness constant k.
+     */
     public Spring(double length, double strength) {
-        this.length = length;
-        this.strength = strength;
         assert length > 0;
         assert strength > 0;
+        this.length = length;
+        this.strength = strength;
     }
     
+    /**
+     * Calculate the spring's currently exerted force.
+     * @param distance The current (positive) length l of the spring.
+     * @return The force (positive force is directed outward).
+     */
     public double getForce(double distance) {
         return (length - distance)*strength;
     }
