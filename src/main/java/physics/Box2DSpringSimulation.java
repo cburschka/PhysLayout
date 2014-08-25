917dc1dde3653d6c3c3d3b710968c503d2e7783c
Christoph Burschka <christoph@burschka.de>
2014-08-25 22:26:47 +0200

    Properly initialize the Box2D world.
    All objects are supposed to behave as non-colliding
    point masses (for now). This requires the creation
    of a new zero-dimensional Shape with a fixed mass.

--- src/main/java/physics/Box2DSpringSimulation.java
+++ src/main/java/physics/Box2DSpringSimulation.java
@@ -8,6 +8,7 @@ import javafx.scene.Node;
 import layout.PhysLayout;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.World;
 
 /**
@@ -31,11 +32,13 @@ public class Box2DSpringSimulation {
 
         // New zero-gravity world:
         world = new World(new Vec2(0, 0));
+        BodyDef def = new BodyDef();
 
         layout.getNodes().stream().forEach((node) -> {
-            // TODO: Create each body.
-            // (with zero volume to avoid collision)
-            Body body = new Body(null, world);
+            def.position.set((float) node.getLayoutX(), (float) node.getLayoutY());
+            Body body = world.createBody(def);
+            // Attach an infinitely dense point mass to the body:
+            body.createFixture(new ShapelessShape((layout.getMass(node))), Float.POSITIVE_INFINITY);
             bodies.put(node, body);
         });
     }
