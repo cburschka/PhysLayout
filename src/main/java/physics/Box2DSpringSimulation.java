c3a264f395a6444bd34ee500966eaf613b110f95
Christoph Burschka <christoph@burschka.de>
2014-09-26 13:05:14 +0200

    Increase timestep back up to 5ms.

--- src/main/java/physics/Box2DSpringSimulation.java
+++ src/main/java/physics/Box2DSpringSimulation.java
@@ -33,7 +33,7 @@ public class Box2DSpringSimulation {
     private final World world;
     private double friction = 0.5;
     private AnimationTimer animation;
-    private long timeStep = (long) 1e5, timeStamp = 0;
+    private long timeStep = (long) 5e6, timeStamp = 0;
     private static final int ITER_VELOCITY = 6, ITER_POS = 3;
     private final ReadOnlyBooleanWrapper running = new ReadOnlyBooleanWrapper(false);
 
